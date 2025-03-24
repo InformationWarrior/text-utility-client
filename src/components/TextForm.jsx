0d383c6da79a83373a61e88c3b08afe3dc9bdc81
Abhishek Sharma <abhishekjps19@gmail.com>
2025-03-24 10:41:41 +0530

    code push

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -25,24 +25,35 @@ function TextForm(props) {
   };
 
   return (
-    <div className="container my-3">
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container my-3">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowerClick}>
+          Convert to Lowercase
+        </button>
       </div>
-      <button className="btn btn-primary mx-2" onClick={handleUpClick}>
-        Convert to Uppercase
-      </button>
-      <button className="btn btn-primary" onClick={handleLowerClick}>
-        Convert to Lowercase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>
+          {text.split(" ").length - 1} words and {text.length} characters.
+        </p>
+        <p>{(text.split(" ").length - 1) * 0.008} minutes read.</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
 
